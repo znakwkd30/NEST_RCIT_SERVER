d1c09fe354c02678ae3e7f53e7a4a20baa2f5e6f
HyeonJun <41042989+znakwkd30@users.noreply.github.com>
2020-11-10 10:39:57 +0900

    [ADD] DTO, Type 추가

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,11 +1,20 @@
+import { IsEnum, IsNotEmpty, IsString } from "class-validator";
 import { Position } from "../entities/user.entity";
 
 export class CreateUserDto {
+    @IsNotEmpty()
+    @IsString()
     id: string;
     
+    @IsString()
+    @IsNotEmpty()
     name: string;
 
+    @IsString()
+    @IsNotEmpty()
     password: string;
 
+    @IsEnum(Position)
+    @IsNotEmpty()
     position: Position;
 }
