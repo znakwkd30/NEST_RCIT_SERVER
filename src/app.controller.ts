76f689126eb40e2f166991dc86efbe63c544b27e
HyeonJun <41042989+znakwkd30@users.noreply.github.com>
2020-11-16 16:52:10 +0900

    [UPDATE] e2e test code

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AuthService } from './auth/auth.service';
 import { LoginUserDto } from './user/dto/login-user.dto';
 
@@ -7,6 +7,12 @@ export class AppController {
   constructor(
     private authService: AuthService,
   ) {}
+
+  @Get('')
+  main() {
+    return "RCIT SERVER Running~"
+  }
+
 
   @Post('auth/login')
   async login (@Body() user: LoginUserDto) {
