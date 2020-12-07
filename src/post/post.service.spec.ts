fd48312644072d0e86c5a48bd452c1886f9ae098
HyeonJun <41042989+znakwkd30@users.noreply.github.com>
2020-12-07 18:02:06 +0900

    [UPDATE] Array -> Object
    오류 수정

--- src/post/post.service.spec.ts
+++ src/post/post.service.spec.ts
@@ -98,7 +98,7 @@ describe('PostService', () => {
     test('should return instance of post', () => {
       const post = postService.getPost(1);
 
-      expect(post).toBeInstanceOf(Array);
+      expect(post).toBeInstanceOf(Object);
     });
 
     test('should return not found exception', () => {
